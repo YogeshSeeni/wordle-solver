eb8379dec5a48a2975a916fc74f2908a69a6829f
YogeshSeeni <yogeshseeni60@gmail.com>
2022-02-06 13:56:54 -0500

    included text of all 5 letter words, and converted to array

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import { Button } from "@mui/material";
 import { FormGroup } from "@material-ui/core";
 import React, { useState } from 'react';
 import Word from "./components/Word";
+import raw from "./words.txt";
+let allWords = []
+fetch(raw)
+  .then(r => r.text())
+  .then(text => {
+    allWords = text.split("\n")
+  });
 function App() {
-  const [words, setWords] = useState([{value: "opera", color: [0,1,0,2,0], i: 0}, {value: "lorem", color: [0,1,0,2,0], i: 1}, {value: "quicky", color: [0,1,0,2,0], i: 2}]);
+  const [words, setWords] = useState([{value: "opera", color: [0,0,0,0,0], i: 0}]);
+  
   const wo = words.map((word) => 
   <Word words={words} setWords={setWords} index={word.i} />
   )
-
+  
+  function makePrediction() {
+    console.log(allWords[2]);
+  }
   return (
     <div className="App">
       {wo}
-      <Button variant="contained"  disableElevation>
+      <Button variant="contained"  onClick={makePrediction} disableElevation>
       Add
       </Button>
     </div>
