b56b7cf46c5196adba49dc92084b03a904ff2dd0
YogeshSeeni <yogeshseeni60@gmail.com>
2022-02-06 13:35:22 -0500

    Made buttons clickable to change color

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { FormGroup } from "@material-ui/core";
 import React, { useState } from 'react';
 import Word from "./components/Word";
 function App() {
-  const [words, setWords] = useState([{value: "opera", color: [0,1,0,2,0]}]);
+  const [words, setWords] = useState([{value: "opera", color: [0,1,0,2,0], i: 0}, {value: "lorem", color: [0,1,0,2,0], i: 1}, {value: "quicky", color: [0,1,0,2,0], i: 2}]);
+  const wo = words.map((word) => 
+  <Word words={words} setWords={setWords} index={word.i} />
+  )
 
   return (
     <div className="App">
-      <Word words={words} index={0} />
+      {wo}
       <Button variant="contained"  disableElevation>
       Add
       </Button>
--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -5,22 +5,55 @@ import { Button } from "@mui/material";
 export default function Word(props) {
   let letters = [];
   for (let i = 0; i < 5; i++) {
-    let tmp = [props.words[props.index].value.charAt(i)]
+    let tmp = [props.words[props.index].value.charAt(i)];
     if (props.words[props.index].color[i] == 0) {
-      tmp.push("gray")
+      tmp.push("gray");
     } else if (props.words[props.index].color[i] == 1) {
-      tmp.push("yellow")
+      tmp.push("yellow");
     } else {
-      tmp.push("green")
+      tmp.push("green");
     }
-    letters.push(tmp)
+    tmp.push(i);
+    letters.push(tmp);
   }
-  const boxes = letters.map((letter) =>
-      <Box bgcolor={letter[1]} width={100} height={100} m={1} clone>
-        <Button style={{ fontSize: "63px", color: "white" }}>
-          {letter[0]}
-        </Button>
-      </Box>
+  function handleClick(i) {
+    let tmp = [...props.words];
+    if (tmp[props.index].color[i] == 2) {
+      tmp[props.index].color[i] = 0
+    } else {
+      tmp[props.index].color[i] += 1
+    }
+    props.setWords(tmp)
+  }
+  const boxes = letters.map((letter) => (
+    <Box m={1}>
+      <Button
+      style={{
+        fontSize: "63px",
+        color: "black",
+        backgroundColor: letter[1],
+        maxWidth: "100px",
+        maxHeight: "100px",
+        minWidth: "100px",
+        minHeight: "100px",
+        margin: "5"
+      }}
+      onClick={() => handleClick(letter[2])}
+    >
+      {letter[0]}
+    </Button>
+    </Box>
+    
+  ));
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {boxes}
+    </div>
   );
-  return <div>{boxes}</div>;
 }
