5c0b0d811078ed858acaa39d4b1d0c4c156583ac
YogeshSeeni <yogeshseeni60@gmail.com>
2022-02-06 14:44:15 -0500

    Created Prediction Algorithm

--- src/App.js
+++ src/App.js
@@ -12,13 +12,84 @@ fetch(raw)
   });
 function App() {
   const [words, setWords] = useState([{value: "opera", color: [0,0,0,0,0], i: 0}]);
-  
+  let green = [];
+  let yellow = [];
+  let gray = [];
+
   const wo = words.map((word) => 
   <Word words={words} setWords={setWords} index={word.i} />
   )
   
+  function populateColors() {
+    green = []
+    yellow = []
+    gray = []
+    let t = []
+    words.forEach((word) => {
+      for (let j = 0; j < 5; j++) {
+        if (word.color[j] == 0) {
+          gray.push(word.value.charAt(j));
+        } else if (word.color[j] == 1) {
+          yellow.push(word.value.charAt(j));
+        } else {
+          green.push([word.value.charAt(j), j]);
+        }
+      }
+    })
+    for (let g = 0; g < gray.length; g++) {
+      if (green.includes(gray[g]) || yellow.includes(gray[g])) {
+        continue;
+      } else {
+        t.push(gray[g]);
+      }
+    }
+    gray = t;
+  }
+
   function makePrediction() {
-    console.log(allWords[2]);
+    populateColors();
+    for (let word = 0; word < allWords.length; word++) {
+      let b = false;
+      for (let g = 0; g < gray.length; g++) {
+        if (allWords[word].includes(gray[g])) {
+          b = true;
+          break;
+        }
+      }
+      if (b) {
+        continue;
+      }
+      for (let y = 0; y < yellow.length; y++) {
+        if (allWords[word].includes(yellow[y]) == false) {
+          b = true;
+          break;
+        }
+      }
+      if (b) {
+        continue;
+      }
+      for (let g = 0; g < green.length; g++) {
+        if (allWords[word].charAt(green[g][1]) != green[g][0]) {
+          b = true;
+          break;
+        }
+      }
+      if (b) {
+        continue;
+      }
+      for (let w = 0; w < words.length; w++) {
+        if (words[w].value == allWords[word]) {
+          b = true;
+          break;
+        }
+      }
+      if (b) {
+        continue;
+      }
+      setWords([...words, {value: allWords[word], color: [0,0,0,0,0], i: words.length}])
+      break;
+    }
+    
   }
   return (
     <div className="App">
